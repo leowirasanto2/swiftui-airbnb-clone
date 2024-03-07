c658ada38873a0892fae927a6f0c3756806d6f9b
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-03-07 17:30:34 +0800

    feature: - implement viewModifier for collapsible view to reduce duplicate lines

--- swiftui-clone-airbnb/Core/Explore/DestinationSearchView.swift
+++ swiftui-clone-airbnb/Core/Explore/DestinationSearchView.swift
@@ -23,16 +23,29 @@ struct DestinationSearchView: View {
     
     var body: some View {
         VStack {
-            
-            Button {
-                withAnimation(.snappy) {
-                    show.toggle()
+            HStack {
+                Button {
+                    withAnimation(.snappy) {
+                        show.toggle()
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
                 }
-            } label: {
-                Image(systemName: "xmark.circle")
-                    .imageScale(.large)
+                
+                Spacer()
+                
+                if !destination.isEmpty {
+                    Button("Clear") {
+                        destination = ""
+                    }
                     .foregroundStyle(.black)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                }
             }
+            .padding()
             
             VStack(alignment: .leading) {
                 if selectedOption == .location {
@@ -58,12 +71,8 @@ struct DestinationSearchView: View {
                     CollapsedPickerView(title: "Where", description: "Add destination")
                 }
             }
-            .padding()
+            .modifier(CollapsibleDestinationViewModifier())
             .frame(height: selectedOption == .location ? 120 : 64)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
             .onTapGesture {
                 withAnimation(.snappy) {
                     selectedOption = .location
@@ -90,12 +99,8 @@ struct DestinationSearchView: View {
                     
                 }
             }
-            .padding()
+            .modifier(CollapsibleDestinationViewModifier())
             .frame(height: selectedOption == .dates ? 180 : 64)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
             .onTapGesture {
                 withAnimation(.snappy) {
                     selectedOption = .dates
@@ -117,27 +122,37 @@ struct DestinationSearchView: View {
                         guard numGuests > 0 else { return }
                         numGuests -= 1
                     }
+                    
                 } else {
                     CollapsedPickerView(title: "Who", description: "Add guests")
                 }
             }
-            .padding()
+            .modifier(CollapsibleDestinationViewModifier())
             .frame(height: selectedOption == .guests ? 120 : 64)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .padding()
-            .shadow(radius: 10)
             .onTapGesture {
                 withAnimation(.snappy) {
                     selectedOption = .guests
                 }
             }
+            
+            Spacer()
         }
     }
 }
 
 #Preview {
     DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsibleDestinationViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+    }
 }
 
 struct CollapsedPickerView: View {
